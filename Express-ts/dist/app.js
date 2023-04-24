4f37b91ea6bf2b4f4935c0cce61a9851cb281fe2
R3gardless <pidaoh@g.skku.edu>
2023-04-24 20:27:36 +0900

    [Add] Express with TypeScript - Add Middleware

--- Express-ts/dist/app.js
+++ Express-ts/dist/app.js
@@ -3,9 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("This is Logging Middleware");
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log("This is Som Middleware");
+    next();
+});
 app.get("/", function (req, res) {
-    console.log(req);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("This is Logging Middleware");
+    res.send({ error: "404 Not Found Error" });
 });
 app.listen(8000, function () {
     console.log("Server is on....");
--- Express-ts/src/app.ts
+++ Express-ts/src/app.ts
@@ -3,9 +3,39 @@ import { Cat, CatType } from "./app.model"; // 상대 경로
 
 const app: express.Express = express();
 
+app.use((req, res, next) => {
+  // Middleware = Place on upper router
+  // app.use = Every Router Middleware
+  console.log(req.rawHeaders[1]);
+  console.log("This is Logging Middleware");
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  // app.get URL = Certain Router Middleware
+  console.log("This is Som Middleware");
+  next(); // Move to according url router
+});
+
+// Top-Bottom Router Search
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
+  // router
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  // This Middleware Means Any Router didn't match req URL
+  console.log("This is Logging Middleware");
+  res.send({ error: "404 Not Found Error" });
 });
 
 app.listen(8000, () => {
